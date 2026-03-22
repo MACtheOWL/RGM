e993058cc28c4bd323545421b184432b5f1e4878
Mac <owlmacmungham@gmail.com>
2026-03-22 04:48:34 -0400

    Card Screen, animations, inital setup

---
+++ src/lib/components/ColorPanels.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<!-- =========================
+     SUMMONING JUTSU (CARDS)
+========================= -->
+<div class="SummoningJutsu">
+
+  <div class="scroll crimsonScroll">
+    <div class="scrollTitle">Games</div>
+    <img class="scrollArt" src="images/games.png" alt="Games">
+  </div>
+
+  <div class="scroll azureScroll">
+    <div class="scrollTitle">Apps</div>
+    <img class="scrollArt" src="images/apps.png" alt="Apps">
+  </div>
+
+  <div class="scroll emeraldScroll">
+    <div class="scrollTitle">Ideas</div>
+    <img class="scrollArt" src="images/ideas.png" alt="Ideas">
+  </div>
+
+  <div class="scroll goldScroll">
+    <div class="scrollTitle">Connect</div>
+    <img class="scrollArt" src="images/connect.png" alt="Connect">
+  </div>
+
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import ColorPanels from '$lib/components/ColorPanels.svelte';
+</script>
+
+<ColorPanels />
